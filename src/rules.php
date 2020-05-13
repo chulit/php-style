eaedd28daab93d6bed4bdbdae3804227a63b6a27
abu-abbas <notificationfor.gitlabs@gmail.com>
2020-05-13 10:53:25 +0700

    initial commit

---
+++ src/rules.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+  # All the rules related to the PSR-2 standard are all bundled into the ruleset @PSR2.
+  # This allows us to opt into the standard without having to specify each rule individually.
+  '@PSR2' => true,
+
+  # allows you to specify if you’d like short or long array syntax
+  'array_syntax' => ['syntax' => 'short'],
+
+  # There should be no blank lines before a namespace declaration.
+  'no_blank_lines_before_namespace' => true,
+
+  # Each element of an array must be indented exactly once.
+  'array_indentation' => true,
+
+  # All instances created with new keyword must be followed by braces.
+  'new_with_braces' => true,
+];
